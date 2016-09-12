c2b6b809a2e9b1b39ae96eb031a6b1cd470baa60
Barry Kern <barryk@slalom.com>
2016-09-12 10:15:55 -0500

    changing to default/standard guice DI

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -1,10 +1,11 @@
 package controllers
 
-import play.api.Environment
+import com.google.inject.Inject
+import play.api.{Configuration, Environment}
 import play.api.mvc._
 import shared.SharedMessages
 
-class Application()(implicit environment: Environment) extends Controller {
+class Application @Inject()(implicit config: Configuration) extends Controller {
 
   def index = Action {
     Ok(views.html.index(SharedMessages.itWorks))
